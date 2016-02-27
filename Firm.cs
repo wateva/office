ede59e87f0a0d2c718e190a83f4be7c8bcd4bc31
Vladimir <vdodoka@yandex.ru>
2016-02-27 21:12:54 +0200

    Moved out service data from Emloyee class

--- Firm.cs
+++ Firm.cs
@@ -2,7 +2,21 @@
 
 public class Firm
 {
-	public Firm()
+    protected string[] posiblePositions = new string[6]
+    {
+        "programmer", "designer", "tester", "manager", "director", "booker"
+    };
+
+    protected var paymentDepence = new Dictionary<string, int>()
+    {
+        {"programmer", 50},
+        {"designer", 40},
+        {"tester", 30},
+        {"manager", 20},
+        {"director", 2400},
+        {"booker", 2400}
+    };
+    public Firm()
 	{
 	}
 }
