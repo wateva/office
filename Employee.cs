865882eb855aca725bbc3c2b8740a77c4bef09eb
Vladimir <vdodoka@yandex.ru>
2016-02-27 21:14:08 +0200

    Unnecessary changes in Employee class

--- Employee.cs
+++ Employee.cs
@@ -6,37 +6,32 @@ public class Employee
 {
     private ArrayList positionsList;
     private int paymeent;
+    private int earnedMoney;
     private bool isFixedPayment;
     private int shedule;
-    private string[] posiblePositions = new string[6]
-    {
-        "programmer", "designer", "tester", "manager", "director","booker"
-    };
 
-    private var paymentDepence = new Dictionary<string, int>()
-    {
-        {"programmer", 50},
-        {"designer", 40},
-        {"tester", 30},
-        {"manager", 20},
-        {"director", 2400},
-        {"booker", 2400}
-    };
 	public Employee()
 	{
         var rand = new Random();
 	    this.shedule = rand.Next(40) + 1;
-        //генерирование должностей для сотрудника
-        int posCount = rand.Next(1 , 7);//количество должностей сотрудника
-        positionsList = new ArrayList();
-        int curPos;
+	    this.positionsList = generatePosions(Firm.posiblePositions);
+	}
+
+    private ArrayList generatePosions(string[] positions)
+    {
+        result = new ArrayList();
+        int currentPos;
+        int posCount = rand.Next(1, 7);//количество должностей сотрудника
+
         for (int i = 0; i < posCount; i++)
         {
-            curPos = rand.Next(6);
-            if (this.positionsList.Contains(posiblePositions[curPos]) == false) // должности не должны повторяться
-                this.positionsList[i] = posiblePositions[curPos];
-            if (this.positionsList[i] == "")
+            while (result[i] == null)
+            {
+                currentPos = rand.Next(6);
+                if (this.result.Contains(positions[currentPos]) == false) // должности не должны повторяться
+                    this.result[i] = positions[currentPos];
+            }
         }
-
-	}
+        return result;
+    }
 }
