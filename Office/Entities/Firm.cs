58582ddcf9decef087053f5950e8a52c0a2cbaae
Vladimir <vdodoka@yandex.ru>
2016-02-29 11:29:23 +0200

    Started to implement the workflow

--- Office/Entities/Firm.cs
+++ Office/Entities/Firm.cs
@@ -1,27 +1,47 @@
 ﻿using System.Collections.Generic;
+using System.Collections;
+using System;
 
 namespace Office.Entities
 {
     public class Firm
     {
-        static public Worker[] posiblePositions = new Worker[6]
+        public Worker[] posiblePositions = new Worker[6]
         {
-           new Booker(), new Director(), new Manager(), new Designer(), new Programmer(), 
-           new Tester()
+           new Booker(), new Manager(), new Designer(), new Programmer(), 
+           new Tester(), new Director()
         };
 
-        static public Dictionary<string, int> paymentDepence = new Dictionary<string, int>()
-        {
-            {"programmer", 50},
-            {"designer", 40},
-            {"tester", 30},
-            {"manager", 2000},
-            {"director", 2400},
-            {"booker", 2400}
-        };
+        //sorry for this.
+        public ArrayList listOfEmployees;
+        public ArrayList listOfBookers;
+        public ArrayList listOfDesigners;
+        public ArrayList listOfManagers;
+        public ArrayList listOfProgrammers;
+        public ArrayList listOfTesters;
 
         public Firm()
         {
+            //создать список сотрудников и список для каждой должности кроме директора
+            listOfEmployees = new ArrayList();
+            listOfBookers = new ArrayList();
+            listOfDesigners = new ArrayList();
+            listOfManagers = new ArrayList();
+            listOfProgrammers = new ArrayList();
+            listOfTesters = new ArrayList();
+            
+            var rand = new Random();
+            for (int i = 0; i < rand.Next(10, 101); i++)
+            {
+                //todo:fill list of employees with random count of random workers
+                //todo:pass throught the array of employees and add links on neccessaty object to suitable lists
+                listOfEmployees.Add(new posiblePositions[rand.Next(6)].GetType()); // не работает.
+            }
+        }
+
+        public void workflow()
+        {
+
         }
     }
 }
